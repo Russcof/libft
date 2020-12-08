4feda1cf3e4132b1fa060fbe8bbe64f27e693ceb
Russcof <russcof.lol@gmail.com>
2020-12-08 17:58:20 +0100

    ft_memccpy

---
+++ ft_memccpy.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memccpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mtellal <mtellal@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2020/12/08 16:54:15 by mtellal           #+#    #+#             */
+/*   Updated: 2020/12/08 16:58:28 by mtellal          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memccpy(void *dest, void const *src, int c, size_t n)
+{
+	unsigned char *tab;
+	unsigned char *tab1;
+
+	tab = dest;
+	tab1 = src;
+	while (n--)
+	{
+		if (*tab == c)
+		{
+			return ((void *)(tab + 1));
+		}
+		*tab++;
+	}
+	return (NULL);
+}
